e8e4f6ee351444be381b491bda5911429af4cd4c
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-09-27 17:02:37 +0900

    project4-1

---
+++ project1/project1/main.c
@@ -0,0 +1,22 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    
+    int x, y, z, m;
+    int a, b, c;
+    x = 2;
+    z = 1;
+    a = 3;
+    b = 4;
+    c = 5;
+    
+    y = a*x*x + b*x + c;
+    m = (x+y+z)/3;
+    
+    printf("y=%d, m=%d\n", y, m);
+    
+    return 0;
+
+}
